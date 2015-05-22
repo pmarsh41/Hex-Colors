32cfeff3365bef4a02247361d1bf0e69795f43fe
Phil <marshall.phillip@gmail.com>
2015-05-21 21:51:46 -0400

    adjustments

--- js/app.js
+++ js/app.js
@@ -22,50 +22,85 @@ function app() {
         // start app?
     })
 
-    var h1s = [].slice.call(document.querySelectorAll('h1'));
+    //////////////////////////////////////////
+
+    var h1 = document.querySelector('h1');
+    var h2 = document.querySelector('h2');
+    var div = document.querySelector('div');
+    var body = document.querySelector('body');
+    //console.log(h1+" "+body);
+
+    var colorHist = [];
+    //var colorCounter = 15;
 
 
     setInterval(function() {
+        // set current time
         var d = new Date();
 
-        var parts = [d.getHours(), d.getMinutes(), d.getSeconds()]
+        // get hours, minutes, seconds
+        var parts = [d.getHours(), d.getMinutes(), d.getSeconds()];
 
-        if (parts[2] < 10) {
+        // convert to hexadecimal
+        var color = [];
+        color[0] = (Math.round((parts[0] / 24) * 255)).toString(16);
+        color[1] = (Math.round((parts[1] / 60) * 255)).toString(16);
+        color[2] = (Math.round((parts[2] / 60) * 255)).toString(16);
 
-            //add 0
-
-            parts[2] = "0" + parts[2];
-
-        if (parts[1] < 10) {
-
-            parts[1] = "0" + parts[1];
-
-        if (parts[0] < 10) {
-
-            parts[0] = "0" + parts[0];
-
-        }
-    }
-
-            //parts.forEach(function("0" + [0], [1]){
-
-            //console.log(parts;)
-        }
-
-        h1s.forEach(function(el) {
-            el.innerHTML = parts.join(":")
-
+        color = color.map(function(val) {
+            return (val.length < 2) ? "0" + val : val;
         })
 
-        var mySecs = Math.round((d.getSeconds() / 59) * 255);
-        var myMins = Math.round((d.getMinutes() / 59) * 255);
-        var myHours = Math.round((d.getHours() / 23) * 255);
+        //console.log('R = ' + color[0] + ', G = ' + color[1] + ', B = ' + color[2]);
 
-        //function bgColor()(
-        document.body.style.backgroundColor = "rgb(" + myHours + ',' + myMins + ',' + mySecs + ")";
-        //"rgb( " + 123 + " , " + 123 + " , " + 123 + ")"
-        //"rerrthy" + someVar
-    }, 1 * 1000)
 
+        // add 0s to single digit
+        parts = parts.map(function(val) {
+            if (val < 10) {
+                val = '0' + val;
+            }
+            return val;
+        })
+
+        h2.innerHTML = color.join(':');
+        h1.innerHTML = parts.join(':');
+        //div.style.backgroundColor = '#' + color.join('');
+
+        //body.style.background = 'repeating-radial-gradient(circle,#000,#000 15em, #' + colorHist.join()
+
+        //colorHist.push('#'+color.join('')+' '+colorCounter+'em');
+
+        //CREATE GRADIENT
+
+
+        // put color history in background
+        body.style.background = 'radial-gradient(circle, #' + color.join('') + ', #' + color.join('') + ' .1em' + colorHist.join('') + ')';
+        //console.log(body.style.background = 'repeating-radial-gradient(circle, #000, #000 15em'+colorHist.join('')+')');
+
+        // ems must increase by 3 each iteration
+        colorHist = colorHist.map(function(val, ind) {
+            if (ind % 3 === 1) {
+                val += .1;
+            }
+            return val;
+        })
+
+        if (colorHist.length > 2999) {
+            colorHist.splice(2995, 6);
+        }
+
+                // unshift em, then push number, then current color
+        colorHist.unshift('em');
+        colorHist.unshift(.2);
+        colorHist.unshift(', #' + color.join('') + ' ');
+
+        // push current color + 15em
+        colorHist.unshift('em');
+        colorHist.unshift(.1);
+        colorHist.unshift(', #' + color.join('') + ' ');
+
+
+    }, 1000);
 
 }
+
